a7fc8d9d758b2c95d062422ea80212b7f27e9938
Tibor <tibor736@hotmail.com>
2023-09-11 01:44:55 +0300

    created modal window for clear cart button

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,6 +1,6 @@
-import { clearCart } from '../features/cart/cartSlice';
 import CartItem from './CartItem';
 import { useSelector, useDispatch } from 'react-redux';
+import { openModal } from '../features/modal/modalSlice';
 
 const CartContainer = () => {
 	const dispatch = useDispatch();
@@ -36,7 +36,7 @@ const CartContainer = () => {
 				</div>
 				<button
 					className='btn clear-btn'
-					onClick={() => dispatch(clearCart())}
+					onClick={() => dispatch(openModal())}
 				>
 					clear cart
 				</button>
